790e2d3e94af9435f4b8970d541fe046983cc2b8
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-02-27 18:17:30 +0100

    Fix argument in nf_db_create

--- db/nf_db_create.sh
+++ db/nf_db_create.sh
@@ -1,6 +1,6 @@
 nf_function nf_db_create database "Create an empty database"
 function nf_db_create() {
-  if [[ ! ${!} ]]; then
+  if [[ ! ${1} ]]; then
     echo "Usage: ${0} DATABASE ..."
 
     return 1
