7471d851c97ea6f85f14def489a52518fb7c159d
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-03-03 11:10:27 +0100

    Add nf_k_scale function

--- k8s/nf_k_redis_flushdb.sh
+++ k8s/nf_k_redis_flushdb.sh
@@ -3,12 +3,12 @@ function nf_k_redis_flushdb() {
   NAMESPACE=${1}
 
   if [[ -z ${NAMESPACE} ]]; then
-    echo "Usage: nf_k_redis_flushdb NAMESPACE"
+    echo "Usage: ${0} NAMESPACE"
 
     return 1
   fi
 
   echo "Flushing Redis database in ${NAMESPACE} namespace"
-  kubectl exec -ti -n ${NAMESPACE} ${NAMESPACE}-resources-redis-master-0 -- redis-cli FLUSHDB
+  kubectl exec -ti -n "${NAMESPACE}" "${NAMESPACE}-resources-redis-master-0" -- redis-cli FLUSHDB
   echo "Flushed Redis database in ${NAMESPACE} namespace"
 }
---
+++ k8s/nf_k_scale.sh
@@ -0,0 +1,16 @@
+nf_function nf_k_scale k8s "Scale a pod up or down (default: debug pod)"
+function nf_k_scale() {
+  NAMESPACE=${1}
+  POD=${2:-debug}
+  REPLICAS=${3:-1}
+
+  if [[ -z ${NAMESPACE} ]]; then
+    echo "Usage: ${0} NAMESPACE [POD] [REPLICAS]"
+
+    return 1
+  fi
+
+  echo "Scaling ${POD} pod to ${REPLICAS} in ${NAMESPACE}"
+  kubectl scale deployment "${POD}" --namespace="${NAMESPACE}" --replicas="${REPLICAS}"
+  echo "Scaled ${POD} pod to ${REPLICAS} in ${NAMESPACE}"
+}
