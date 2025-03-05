5e834956a352861d13208967ecb5549f89d6bba3
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-03-05 16:38:44 +0100

    Correct nf_cop_branch function name

--- dev/nf_cop_branch.sh
+++ dev/nf_cop_branch.sh
@@ -1,5 +1,5 @@
 nf_function nf_cop_branch dev "Autocorrect RuboCop offenses in changed files (compared to a branch)"
-function nf_branchcop() {
+function nf_cop_branch() {
   BRANCH=${1:-$(git_main_branch)}
 
   git diff-tree -r --no-commit-id --name-only ${BRANCH}@\{u\} head | xargs ls -1 2>/dev/null | xargs rubocop --autocorrect-all --force-exclusion
