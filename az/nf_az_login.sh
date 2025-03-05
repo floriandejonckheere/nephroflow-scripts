7a25035bf4c39abfe8ea5efe9136b49d828fbdb2
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-03-05 09:50:08 +0100

    Rename nf_az_signin to nf_az_login

--- az/nf_az_login.sh
+++ az/nf_az_login.sh
@@ -1,5 +1,5 @@
-nf_function nf_az_signin az "Sign in to Azure"
-function nf_az_signin() {
+nf_function nf_az_login az "Log in to Azure"
+function nf_az_login() {
   az login --tenant 364d007c-085f-4eac-8af2-138b2a043aea
   az acr login --name nephroflow
 }
