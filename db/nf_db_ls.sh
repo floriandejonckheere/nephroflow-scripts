3ca18b1c65aadfa6d63410c9d7c64b3de4a76f33
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-03-14 09:43:40 +0100

    nf_db_ls: show database size

--- db/nf_db_ls.sh
+++ db/nf_db_ls.sh
@@ -1,4 +1,4 @@
 nf_function nf_db_ls database "List all databases"
 function nf_db_ls() {
-  nf_db -t -c "SELECT datname FROM pg_database WHERE datistemplate = false AND datname <> 'postgres' ORDER BY datname" | tr -s '\n' | sed 's/^ *//'
+  nf_db -t -c "SELECT datname, pg_size_pretty(pg_database_size(datname)) FROM pg_database WHERE datistemplate = false AND datname <> 'postgres' ORDER BY datname" | tr -s '\n' | sed 's/^ *//'
 }
