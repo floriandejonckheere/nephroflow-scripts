9473c7bc11be1ecf0d19db273bfb6a49692ba94c
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-03-25 11:06:16 +0100

    nf_db_load: support plain .sql files

--- db/nf_db_load.sh
+++ db/nf_db_load.sh
@@ -5,7 +5,12 @@ function nf_db_load() {
   DATABASE=${DATABASE:-development}
 
   FILE=${2:-${NF_DB_PREFIX}${DATABASE}}
-  FILE="${FILE%.sql}.sql.gz"
+
+  # Check if the file is a .sql file or a .gz file
+  FILE="${FILE%.sql}.sql"
+  if [[ ! -f "${FILE}" ]]; then
+    FILE="${FILE}.gz"
+  fi
 
   if [[ ! ${DATABASE} ]]; then
     echo "Usage: ${0} DATABASE [FILE]"
@@ -13,9 +18,27 @@ function nf_db_load() {
     return 1
   fi
 
+  if [[ ! -f "${FILE}" ]]; then
+    echo "File ${FILE} not found"
+
+    return 1
+  fi
+
   echo "Loading database ${NF_DB_PREFIX}${DATABASE} from ${FILE}"
 
-  gunzip -c "${FILE}" | nf_compose exec -T postgres psql -U postgres "${NF_DB_PREFIX}${DATABASE}" > /dev/null
+  if [[ "${FILE}" == *.sql ]]; then
+    # If the file is a .sql file, use psql to load it
+    nf_compose exec -T postgres psql -U postgres "${NF_DB_PREFIX}${DATABASE}" < "${FILE}" || {
+      echo "Failed to load database ${NF_DB_PREFIX}${DATABASE} from ${FILE}"
+      return 1
+    }
+  else
+    # If the file is a .gz file, use gunzip to decompress it and then pipe it to psql
+    gunzip -c "${FILE}" | nf_compose exec -T postgres psql -U postgres "${NF_DB_PREFIX}${DATABASE}" > /dev/null || {
+      echo "Failed to load database ${NF_DB_PREFIX}${DATABASE} from ${FILE}"
+      return 1
+    }
+  fi
 
   echo "Database ${NF_DB_PREFIX}${DATABASE} loaded from ${FILE}"
 }
