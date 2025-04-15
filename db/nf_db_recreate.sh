f1b4a7ed74f1fb6e109595c17ec091be1162f0ef
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-04-15 13:59:13 +0200

    Add nf_db_recreate function

---
+++ db/nf_db_recreate.sh
@@ -0,0 +1,13 @@
+nf_function nf_db_recreate database "Drop and recreate a database"
+function nf_db_recreate() {
+  if [[ ! ${1} ]]; then
+    echo "Usage: ${0} DATABASE ..."
+
+    return 1
+  fi
+
+  for DATABASE in "${@}"; do
+    nf_db_drop "${DATABASE}"
+    nf_db_create "${DATABASE}"
+  done
+}
