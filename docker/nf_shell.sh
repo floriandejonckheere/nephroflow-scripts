b32c1796b29d7bc5257c4ea8d03036bc014e33d7
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-04-28 08:18:13 +0200

    nf_shell: expose service ports if not running

--- docker/nf_shell.sh
+++ docker/nf_shell.sh
@@ -1,4 +1,10 @@
-nf_function nf_shell docker "Start a shell in the NephroFlow API container"
+nf_function nf_shell docker "Start a shell in the NephroFlow API container (exposed port 3000 if not running already) "
 function nf_shell() {
-  nf_compose run --rm web bash
+  if docker ps --format '{{.Names}}' | grep -q '^web$'; then
+    echo "Starting shell in web container"
+    nf_compose run --rm web bash
+  else
+    echo "Starting shell in web container (port 3000)"
+    nf_compose run --rm --name web --service-ports web bash
+  fi
 }
