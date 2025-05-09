f74e6479ee42a84107b8254ca4f91b7e0e2cd16b
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-05-09 14:14:21 +0200

    nf_api: set container name

--- docker/nf_api.sh
+++ docker/nf_api.sh
@@ -1,4 +1,4 @@
 nf_function nf_api docker "Start NephroFlow API server"
 function nf_api() {
-  nf_compose run --rm --service-ports web rails s -b 0.0.0.0
+  nf_compose run --rm --name web --service-ports web rails s -b 0.0.0.0
 }
