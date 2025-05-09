6fee1c1c0cbf8985407fee019c45996c92f5abaa
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-05-09 13:54:08 +0200

    Merge nf_rel_bump into nf_bump

--- dev/nf_bump.sh
+++ dev/nf_bump.sh
@@ -1,22 +1,37 @@
 nf_function nf_bump dev "Bump (patch) version in CHANGELOG.md"
 function nf_bump() {(set -euo pipefail
-  # Parse current version
-  VERSION=$(grep '^\[unreleased\]' CHANGELOG.md | sed -e 's/.*compare\/v\([0-9]*\).\([0-9]*\).\([0-9]*\).*/\1.\2.\3/g')
+  REPOSITORY=${1:-$(basename "${PWD}")}
 
-  # Split version into major, minor, and patch
-  MAJOR=$(echo "${VERSION}" | cut -d '.' -f 1)
-  MINOR=$(echo "${VERSION}" | cut -d '.' -f 2)
-  PATCH=$(echo "${VERSION}" | cut -d '.' -f 3)
-  OLD_PATCH=${PATCH}
+  # Get today's date in YYYY-MM-DD format
+  DATE=$(date +"%Y-%m-%d")
 
-  # Increment patch
-  PATCH=$((PATCH + 1))
+  # Extract the current version from CHANGELOG.md
+  CURRENT_VERSION=$(grep -E "^## \[[0-9]+\.[0-9]+\.[0-9]+\]" "${NF_PATH}/${REPOSITORY}/CHANGELOG.md" | head -1 | sed -E 's/^## \[([0-9]+\.[0-9]+\.[0-9]+)\].*/\1/')
+
+  if [[ -z ${CURRENT_VERSION} ]]; then
+    echo "Could not find version number in CHANGELOG.md, parsing version.rb"
+
+    CURRENT_VERSION="$(nf_version).0"
+  fi
+
+  if [[ -z ${CURRENT_VERSION} ]]; then
+    echo "Error: Could not find a version number in CHANGELOG.md"
+
+    return 1
+  fi
+
+  # Split the version into major, minor, patch
+  IFS='.' read -r MAJOR MINOR PATCH <<< "${CURRENT_VERSION}"
+
+  # Increment the patch version
+  NEW_PATCH=$((PATCH + 1))
+  NEW_VERSION="${MAJOR}.${MINOR}.${NEW_PATCH}"
 
   # Write section
-  gsed -i -e "s/^## \[Unreleased\]/## \[Unreleased\]\n\n## \[${MAJOR}.${MINOR}.${PATCH}] - $(date '+%Y-%m-%d')/" CHANGELOG.md
+  gsed -i -e "s/^## \[Unreleased\]/## \[Unreleased\]\n\n## \[${NEW_VERSION}] - ${DATE}/" "${NF_PATH}/${REPOSITORY}/CHANGELOG.md"
 
   # Write hyperlinks
-  gsed -i -e "s/^\[unreleased\].*/\[unreleased\]: https:\/\/github.com\/$(nf_repo | sed -e 's/\//\\\//g')\/compare\/v${MAJOR}.${MINOR}.${PATCH}..release\/v${MAJOR}.${MINOR}\n\[${MAJOR}.${MINOR}.${PATCH}\]: https:\/\/github.com\/$(nf_repo | sed -e 's/\//\\\//g')\/compare\/v${MAJOR}.${MINOR}.${OLD_PATCH}..v${MAJOR}.${MINOR}.${PATCH}/" -i CHANGELOG.md
+  gsed -i -e "s/^\[unreleased\].*/\[unreleased\]: https:\/\/github.com\/$(nf_repo | sed -e 's/\//\\\//g')\/compare\/v${NEW_VERSION}..release\/v${MAJOR}.${MINOR}\n\[${NEW_VERSION}\]: https:\/\/github.com\/$(nf_repo | sed -e 's/\//\\\//g')\/compare\/v${CURRENT_VERSION}..v${NEW_VERSION}/" -i "${NF_PATH}/${REPOSITORY}/CHANGELOG.md"
 
-  echo "Bumped version to ${MAJOR}.${MINOR}.${PATCH}"
+  echo "Bumped CHANGELOG.md from ${CURRENT_VERSION} to version ${NEW_VERSION}"
 )}
